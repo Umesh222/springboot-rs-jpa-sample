243a33ed17df4f024dcc00d6b7d961c647c55566
Umesh222 <46746007+Umesh222@users.noreply.github.com>
2021-12-10 16:37:57 +0530

    testing is done

---
+++ src/main/java/com/springbootrsjpasample/springbootrsjpasample/SpringbootRsJpaSampleApplication.java
@@ -0,0 +1,14 @@
+package com.springbootrsjpasample.springbootrsjpasample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRsJpaSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRsJpaSampleApplication.class, args);
+		//System.out.println("spring boot first application running success....");
+	}
+
+}
---
+++ src/test/java/com/springbootrsjpasample/springbootrsjpasample/SpringbootRsJpaSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootrsjpasample.springbootrsjpasample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRsJpaSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
